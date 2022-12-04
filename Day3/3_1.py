41b3ada39624e5ef52d983465eb48424f437e217
Druvy <nick.goris@student.hu.nl>
2022-12-04 08:40:54 +0100

    Day4

--- Day3/3_1.py
+++ Day3/3_1.py
@@ -1,13 +1,1 @@
-#with open("input.txt") as f:
-#    lines = f.readlines()
-
-#(lambda x:(ord(x)-ord('a')+1) if x.islower() else (ord(x)-ord('A')+27))
-
 print(sum([(lambda x:(ord(x)-ord('a')+1) if x.islower() else (ord(x)-ord('A')+27))(list(set(line[slice(0,len(line)//2)])&set(line[slice(len(line)//2,len(line))]))[0]) for line in open("input.txt").readlines()])) 
-#for line in lines:
-#    n = len(line)
-#    s1 = slice(0,n//2)
-#    s2 = slice(n//2,n)
-#    res.append(convert_to_priority(list(set(line[s1])&set(line[s2]))[0]))
-#print(sum(res))
-    
