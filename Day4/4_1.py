a641bb7904fa88e03648d0c69ec2f8c58c441f68
Druvy <nick.goris@student.hu.nl>
2022-12-06 13:11:39 +0100

    .

--- Day4/4_1.py
+++ Day4/4_1.py
@@ -1,8 +1,6 @@
 with open("input.txt") as f:
     lines = f.readlines()
 
-test = ["2-4,6-8","2-3,4-5","5-7,7-9","2-8,3-7","6-6,4-6","2-6,4-8"]
-
 split = [pair.strip().split(',') for pair in lines]
 split = [[pair[0].split('-'),pair[1].split('-')] for pair in split]
 print(split)
--- Day6/6_1.py
+++ Day6/6_1.py
@@ -1,7 +1,15 @@
 with open("input.txt") as f:
     line = f.read()
-    
+ 
+for i in range(4,len(line)):
+    if len(set(line[i-4:i])) == 4:
+        print(f'6_1: {i}')
+        break
+ 
 for i in range(14,len(line)):
     if len(set(line[i-14:i])) == 14:
-        print(i)
+        print(f'6_2: {i}')
         break
+
+
+      
